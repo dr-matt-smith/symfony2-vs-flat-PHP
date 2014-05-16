e7e8a2f69ee1cfd3bf7a11e0411626a0f74c1e4f
Matt Smith <dr_matt_smith@me.com>
2014-05-16 04:32:19 +0100

    v2.0.4 - removed some comments from code - to reduce SensioInsight violations

--- matt1/views/INCLUDE_html_template.php
+++ matt1/views/INCLUDE_html_template.php
@@ -1,9 +1,5 @@
 <?php
 
-//$htmlHeaderPath = "$templatePath/INCLUDE_html_header.php";
-//$htmlNavPath = "$templatePath/INCLUDE_html_nav.php";
-//$htmlFooterPath = "$templatePath/INCLUDE_html_footer.php";
-
 $htmlHeaderPath = "INCLUDE_html_header.php";
 $htmlNavPath = "INCLUDE_html_nav.php";
 $htmlFooterPath = "INCLUDE_html_footer.php";
